f08e165388605dd5267634aa806a92f3afa97072
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 00:41:38 +0800

    List of 7 Vegetables

--- fruit_list.py
+++ fruit_list.py
@@ -1,9 +1,9 @@
 fruits_names = ["Apple", "Banana", "Orange", "Strawberry", "Blueberry", "Grape", "Mango", "Pineapple", "Watermelon", "Peach", "Kiwi", "Avocado"]
 
-print("a. Entire list: ",fruit_names)
-print("b. 9th index is : ", fruit_names[9])
-fruit_names[2] = "Mango"
-del fruit_names[10]
-print("d. Updated list: ",fruit_names)
-print("e. Sliced portion: ", fruit_names[4:7])
-print("f. Last index is: ", fruit_names[-1])
+print("a. Entire list: ",fruits_names)
+print("b. 9th index is : ", fruits_names[9])
+fruits_names[2] = "Mango"
+del fruits_names[10]
+print("d. Updated list: ",fruits_names)
+print("e. Sliced portion: ", fruits_names[4:7])
+print("f. Last index is: ", fruits_names[-1])
---
+++ vegetables_list.py
@@ -0,0 +1,8 @@
+vegetables_names = ["Broccoli", "Carrot", "Tomato", "Cucumber", "Potato", "Onion", "Bell Pepper", "Green Beans"]
+
+print("a. Entire list: ",vegetables_names)
+print("b. 5th index is : ", vegetables_names[5])
+vegetables_names[3] = "Spinach"
+del vegetables_names[6]
+print("c. Updated list: ",vegetables_names)
+print("d. Last index is: ",vegetables_names[-1])
