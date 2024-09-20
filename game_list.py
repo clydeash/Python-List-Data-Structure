f3cb46a4487ae2d0e9a46a77b6ca3c5a3c561d32
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 01:54:52 +0800

    15 list of my stressreliever games

---
+++ game_list.py
@@ -0,0 +1,9 @@
+game_names = ["Waste collector", "Block blast", "Roblox", "Mobile legends", "Honor of kings", "Call of duty", "Geometry dash", "Candy crush", "Subway surfers", "Zombie tsunami", "Plants vs zombie", "My Talking tom", "8 ball pool", "Granny", "Stick war", "Magic tiles"]
+
+print("a. Entire list: ",game_names)
+print("b. 7th index is : ", game_names[7])
+game_names[4] = "Minecraft"
+del game_names[9]
+print("c. Updated list: ",game_names)
+print("d. Sliced portion: ", game_names[5:10])
+print("e. Last index is: ", game_names[-1])
