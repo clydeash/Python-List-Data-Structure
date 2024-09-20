6e62204ec8d1477f3b76016058bd7312ecf0c4fe
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 01:03:01 +0800

    List of my 20 favorite movies

---
+++ movie_list.py
@@ -0,0 +1,9 @@
+movie_names = ["Iron Man", "Captain America", "The Incredible Hulk", "Doctor Strange", "Ant-man", "Black Widow", "Captain Marvel", "Thor Ragnarok", "Black Panther", "Spider-man", "Eternals", "Shang-chi", "I am groot", "Agent carter", "Moon knight", "X-men", "Deadpool", "Batman", "Superman", "Avengers Endgame", "Loki"]
+
+print("a. Entire list: ",movie_names)
+print("b. 12th index is : ",movie_names[12])
+movie_names[15] = "Inception"
+del movie_names[18]
+print("d. Updated list: ",movie_names)
+print("e. Sliced portion: ", movie_names[8:13])
+print("f. Last index is: ", movie_names[-1])
