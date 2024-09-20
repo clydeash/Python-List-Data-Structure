e302eda70d73e47f3ad50b105847281cbf3acbe8
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-20 23:20:18 +0800

    list of 20 students

---
+++ studentlist.py
@@ -0,0 +1,9 @@
+Student_names = ["Vincent", "Carlos", "Angelo", "Jeruel", "Michael", "James", "Aljon", "Ram", "Kyan", "Julius", "Alex", "Joshua", "Carl", "Janus", "Rodney", "Leonardo", "Charles", "Elijah", "Jhay", "Justine"]
+
+print("a. Entire list: ", Student_names)
+print("b. 15th index is : ", Student_names[14])
+Student_names[11] = "Gabriel"
+del Student_names[9]
+print("d. Updated list: ", Student_names)
+print("e. Sliced portion: ", Student_names[2:5])
+print("f. Last index is: ", Student_names[-1])
