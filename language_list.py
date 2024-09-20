2737065bb1f250da1e6a17e12c33cdcb10ca024c
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 02:21:52 +0800

    List of 14 programming languages

---
+++ language_list.py
@@ -0,0 +1,9 @@
+language_names = ["Javascript", "Kotlin", "C#", "Swift", "C++", "Ruby", "Sql", "Html", "Css", "Nosql", "Rust", "Perl", "Php", "Go", "Matlab", "Scala"]
+
+print("a. Entire list: ",language_names)
+print("b. 11th index is : ", language_names[11])
+language_names[9] = "Python"
+del language_names[13]
+print("c. Updated list: ",language_names)
+print("d. Sliced portion: ", language_names[5:12])
+print("e. Last index is: ", language_names[-1]) 
