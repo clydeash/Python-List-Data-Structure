ab8862799d23a20de8c88f2e183fdcbe7aa8b935
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 00:05:19 +0800

    List of 15 colors

---
+++ color_list.py
@@ -0,0 +1,9 @@
+color_names = ["Red", "Blue", "Green", "Yellow", "Cyan", "Orange", "Pink", "Brown", "Black", "White", "Silver", "Gold", "Teal", "Indigo", "Magenta"]
+ 
+print("a. Entire list: ",color_names)
+print("b. 10th index is : ", color_names[10])
+color_names[4] = "Purple"
+del color_names[5]
+print("d. Updated list: ",color_names)
+print("e. Sliced portion: ", color_names[2:8])
+print("f. Last index is: ", color_names[-1])
