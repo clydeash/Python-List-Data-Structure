9e46f44b9905077abc91fd9438244a0337ef221c
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 02:33:56 +0800

    List of 10 fragrant flowers

---
+++ flower_list.py
@@ -0,0 +1,8 @@
+flower_names = ["Sunflower", "Lily", "Tulip", "Daisy", "Orchid", "Carnation", "Hydrangea", "Iris", "Lavender", "Magnolia", "Daffofil"]
+
+print("a. Entire list: ",flower_names)
+print("b. 5th index is : ", flower_names[5])
+flower_names[7] = "Rose"
+del flower_names[2]
+print("c. Updated list: ",flower_names)
+print("e. Last index is: ", flower_names[-1])
