be08e5f3d28743eaf261a623abd1e1226128ab47
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 02:06:38 +0800

    List of 8 planets in the solar system

---
+++ planet_list.py
@@ -0,0 +1,8 @@
+planet_names = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+print("a. Entire list: ",planet_names)
+print("b. 3rd index is : ", planet_names[3])
+planet_names[7] = "Pluto"
+del planet_names[4]
+print("c. Updated list: ",planet_names)
+print("e. Last index is: ", planet_names[-1])
