6ad3036283c5e938c40354e23c8b788ea71f3475
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 00:23:34 +0800

    List of 12 Fruits

---
+++ fruit_list.py
@@ -0,0 +1,9 @@
+fruits_names = ["Apple", "Banana", "Orange", "Strawberry", "Blueberry", "Grape", "Mango", "Pineapple", "Watermelon", "Peach", "Kiwi", "Avocado"]
+
+print("a. Entire list: ",fruit_names)
+print("b. 9th index is : ", fruit_names[9])
+fruit_names[2] = "Mango"
+del fruit_names[10]
+print("d. Updated list: ",fruit_names)
+print("e. Sliced portion: ", fruit_names[4:7])
+print("f. Last index is: ", fruit_names[-1])
