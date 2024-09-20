57e07a6e1e212adfd91c5d6e28fae101005af536
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-20 23:24:31 +0800

    list of 10 animals

---
+++ animal_list.py
@@ -0,0 +1,10 @@
+
+animal_names = ["Lion", "Tiger", "Bear", "Wolf", "Fox", "Deer", "Horse", "Zebra", "Giraffe", "Monkey"]
+
+print("a. Entire list: ", animal_names)
+print("b. 3rd index is : ", animal_names[2])
+animal_names[5] = "Elephant"
+print("c. Updated list: ",animal_names)
+del animal_names[8]
+print("d. Updated list: ",animal_names)
+print("f. Last index is: ",animal_names[-1])
