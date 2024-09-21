a003305aa69b91dcd8a046b30aed1f1482ad8c35
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 14:59:00 +0800

    The list of 12 furniture items in the house

---
+++ furniture_list.py
@@ -0,0 +1,10 @@
+furniture_list = ["Table", "Chair", "Bed", "Desk", "Cabinet", "Shelf", "Dresser", "Wardrobe", "Ottoman", "Mirror", "Lamp", "Rug"]
+
+print("a. Entire list: ", furniture_list)
+print("b. 8th index is: ", furniture_list[7])
+
+furniture_list[4] = "Sofa"
+del furniture_list[10]
+
+print("c. Updated list: ", furniture_list)
+print("d. Last index is: ", furniture_list[-1])
