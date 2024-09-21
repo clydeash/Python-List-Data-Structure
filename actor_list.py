2a62d24b22c56f40059e2b1f850d99ba86a26a46
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 15:29:40 +0800

    The list of 10 famous celebrity in HollyWood

---
+++ actor_list.py
@@ -0,0 +1,8 @@
+actor_list = ["Tom Hanks", "Meryl Streep", "Brad Pitt", "Jennifer Lawrence", "Denzel Washington", "Julia Roberts", "Tommy Lee Jones", "Angelina Jolie", "Will Smith", "Cate Blanchett"]
+
+print("a. Entire list: ", actor_list)
+print("b. 7th index is: ", actor_list[6])
+actor_list[2] = "Leonardo DiCaprio"
+del actor_list[7]
+print("c. Updated list: ", actor_list)
+print("d. Last index is: ", actor_list[-1])
