6c3fd0cfc779bd4edb2f25c7c8b15efd2cd21f20
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 14:24:08 +0800

    The list of 12 holidays in 365 days

---
+++ holiday_list.py
@@ -0,0 +1,9 @@
+holiday_list = ["New Year's Day", "Valentine's Day", "Easter", "Labor Day", "Halloween", "Thanksgiving", "Independence Day", "Christmas Day", "Hanukkah", "Diwali", "Ramadan", "Chinese New Year"]
+
+print("a. Entire list: ", holiday_list)
+print("b. 9th index is: ", holiday_list[8])
+holiday_list[2] = "Christmas"
+del holiday_list[10]
+print("c. Updated list: ", holiday_list)
+print("d. Sliced portion: ", holiday_list[2:8])
+print("e. Last index is: ", holiday_list[-1])
