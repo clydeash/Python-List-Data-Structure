2917b55397393ff785714a498c597d7f0e365f1c
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 20:53:52 +0800

    List of 10 Festival Celebration

---
+++ festival_celebration_list.py
@@ -0,0 +1,10 @@
+festival_celebration_list = ["Christmas", "Halloween", "Diwali", "Easter", "Ramadan", "New Year's Eve", "Valentine's Day", "Hanukkah", "Oktoberfest", "Carnival"]
+
+print("a. Entire list: ", festival_celebration_list)
+print("b. 5th index is: ", festival_celebration_list[4])
+
+festival_celebration_list[2] = "Thanksgiving"
+del festival_celebration_list[6]
+
+print("c. Updated list: ", festival_celebration_list)
+print("d. Last index is: ", festival_celebration_list[-1])
