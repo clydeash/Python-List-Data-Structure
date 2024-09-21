e64fc238e4f399957a2d9e2bbe02eed96e8fd8b4
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 21:29:42 +0800

    The list of 10 natural landmarks in the world

---
+++ natural_landmarks_list.py
@@ -0,0 +1,10 @@
+natural_landmark_list = ["Great Barrier Reef", "Mount Everest", "Victoria Falls", "Sahara Desert", "Northern Lights", "Paricutin Volcano", "Galapagos Islands", "Amazon Rainforest", "Yellowstone National Park", "Niagara Falls"]
+
+print("a. Entire list: ", natural_landmark_list)
+print("b. 8th index is: ", natural_landmark_list[7])
+
+natural_landmark_list[4] = "Grand Canyon"
+del natural_landmark_list[8]
+
+print("c. Updated list: ", natural_landmark_list)
+print("d. Last index is: ", natural_landmark_list[-1])
