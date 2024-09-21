dcb7626a87bb8b4b51238afc22f52cd7610364e5
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 15:11:01 +0800

    List of 10 Good and famous sports

---
+++ sports_list.py
@@ -0,0 +1,10 @@
+sports_list = ["Football", "Tennis", "Swimming", "Golf", "Soccer", "Boxing", "Cricket", "Rugby", "Hockey", "Volleyball"]
+
+print("a. Entire list: ", sports_list)
+print("b. 6th index is: ", sports_list[5])
+
+sports_list[3] = "Basketball"
+del sports_list[8]
+
+print("c. Updated list: ", sports_list)
+print("d. Last index is: ", sports_list[-1])
