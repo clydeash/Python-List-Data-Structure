ae8632410bd4f90ec18b275856f27a98b61de040
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 20:22:34 +0800

    List of 15 Delicious Recipes

---
+++ recipe_list.py
@@ -0,0 +1,9 @@
+recipe_list = ["Pasta Carbonara", "Chicken Stir-Fry", "Beef Tacos", "Vegetable Curry", "Grilled Salmon", "Chocolate Cake", "Caesar Salad", "Apple Pie", "Honey Chicken", "Ratatouille", "Shrimp Scampi", "Mushroom Risotto", "Chicken Parmesan", "Tiramisu", "Lemon Bars"]
+
+print("a. Entire list: ", recipe_list)
+print("b. 12th index is: ", recipe_list[11])
+recipe_list[8] = "Lasagna"
+del recipe_list[10]
+print("c. Updated list: ", recipe_list)
+print("d. Sliced portion: ", recipe_list[4:10])
+print("e. Last index is: ", recipe_list[-1])
