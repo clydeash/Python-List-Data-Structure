ab0200a1555208b0953406c9052ed5bc677e6fd9
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 14:27:14 +0800

    The list of my 10 favorite desserts

---
+++ dessert_list.py
@@ -0,0 +1,8 @@
+dessert_list = ["Brownie", "Ice Cream", "Cheesecake", "Apple Pie", "Tiramisu", "Pavlova", "Cupcake", "Creme Brulee", "Panna Cotta", "Chocolate Mousse"]
+
+print("a. Entire list: ", dessert_list)
+print("b. 4th index is: ", dessert_list[3])
+dessert_list[5] = "Chocolate Cake"
+del dessert_list[2]
+print("c. Updated list: ", dessert_list)
+print("d. Last index is: ", dessert_list[-1])
