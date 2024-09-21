03ac9e500ff2d4bc26969a791397d9c0a82f29d4
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 15:51:34 +0800

    List of 15 Modern Technology in Time

---
+++ technology_inventions_list.py
@@ -0,0 +1,9 @@
+technology_inventions_list = ["Internet", "Motor Car", "Electric Car", "Artificial Intelligence", "Virtual Reality", "Blockchain", "Drones", "3D Printing", "Augmented Reality", "Robotic Surgery", "Self-Driving Cars", "Wireless Charging", "Smart Home Technology", "SpaceX Rockets", "Biometric Security"]
+
+print("a. Entire list: ", technology_inventions_list)
+print("b. 11th index is: ", technology_inventions_list[10])
+technology_inventions_list[7] = "Smartphone"
+del technology_inventions_list[13]
+print("c. Updated list: ", technology_inventions_list)
+print("d. Sliced portion: ", technology_inventions_list[4:10])
+print("e. Last index is: ", technology_inventions_list[-1])
