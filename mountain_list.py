15b1920533654279663184d204fc8507a06d83bd
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 09:37:14 +0800

    The list of 8 highest mountain in the world

---
+++ mountain_list.py
@@ -0,0 +1,8 @@
+mountain_names = ["K2", "Kangchenjunga", "Lhotse", "Makalu", "Kilimanjaro", "Mount Denali", "Mount Aconcagua", "Mount Elbrus", "Mount Kosciuszko"]
+
+print("a. Entire list: ",mountain_names)
+print("b. 5th index is : ", mountain_names[5])
+mountain_names[3] = "Everest"
+del mountain_names[6]
+print("c. Updated list: ",mountain_names)
+print("e. Last index is: ", mountain_names[-1])
