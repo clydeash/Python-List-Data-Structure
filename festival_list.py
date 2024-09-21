4f2e1df7e1ef3995adf9e06d906c162cd6e6cb2a
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 14:40:28 +0800

    The list of 15 festival all over the country

---
+++ festival_list.py
@@ -0,0 +1,9 @@
+festival_list = ["Oktoberfest", "Carnival", "Mardi Gras", " The Fuji Rock Festival ", "Chinese New Year", "Songkran", "Holi", "Rio Carnival", "Day of the Dead", "Hanami", "Ramadan", "Christmas", "Eid al-Fitr", "Bastille Day", "Lantern Festival"]
+
+print("a. Entire list: ", festival_list)
+print("b. 7th index is: ", festival_list[6])
+festival_list[10] = "Diwali"
+del festival_list[8]
+print("c. Updated list: ", festival_list)
+print("d. Sliced portion: ", festival_list[5:12])
+print("e. Last index is: ", festival_list[-1])
