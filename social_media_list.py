a8177bccbd04f6180d3c226bfe53b69a715cb906
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 15:04:08 +0800

    10 list of the most famous platform in modern period

---
+++ social_media_list.py
@@ -0,0 +1,10 @@
+social_media_list = ["Facebook", "Twitter", "LinkedIn", "Snapchat", "Pinterest", "YouTube", "TikTok", "Reddit", "WhatsApp", "Tumblr"]
+
+print("a. Entire list: ", social_media_list)
+print("b. 5th index is: ", social_media_list[4])
+
+social_media_list[2] = "Instagram"
+del social_media_list[7]
+
+print("c. Updated list: ", social_media_list)
+print("d. Last index is: ", social_media_list[-1])
