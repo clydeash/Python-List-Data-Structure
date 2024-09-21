fe0e6ba531208f60e62bacc00af465a0e8912442
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 16:09:19 +0800

    The list of 8 Animal Habitats in the Amazon

---
+++ animal_habitats_list.py
@@ -0,0 +1,8 @@
+animal_habitats_list = ["Forest", "Ocean", "Desert", "Jungle", "Lakes and Ponds", "Arctic", "Grassland", "Wetland"]
+
+print("a. Entire list: ", animal_habitats_list)
+print("b. 5th index is: ", animal_habitats_list[4])
+animal_habitats_list[2] = "Savannah"
+del animal_habitats_list[5]
+print("c. Updated list: ", animal_habitats_list)
+print("d. Last index is: ", animal_habitats_list[-1])
