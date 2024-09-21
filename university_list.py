a41fc6c09961145851c7293fd35c64e1b7008bc7
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 20:46:51 +0800

    The List of 10 Most Famous University

---
+++ university_list.py
@@ -0,0 +1,8 @@
+university_list = ["Stanford University", "MIT", "Oxford University", "Yale University", "Cambridge University", "Princeton University", "UC Berkeley", "Caltech", "Columbia University", "University of Chicago"]
+
+print("a. Entire list: ", university_list)
+print("b. 6th index is: ", university_list[5])
+university_list[3] = "Harvard University"
+del university_list[8]
+print("c. Updated list: ", university_list)
+print("d. Last index is: ", university_list[-1])
