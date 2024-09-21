3de5fc9b77baa9ffe8afb4690affa88859715024
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 22:17:43 +0800

    This is the 7 wonders of the world

---
+++ world_wonder_list.py
@@ -0,0 +1,8 @@
+world_wonders_list = ["Great Pyramid of Giza", "Hanging Gardens of Babylon", "Statue of Zeus at Olympia", "Temple of Artemis at Ephesus", "Mausoleum at Halicarnassus", "Colossus of Rhodes", "Lighthouse of Alexandria"]
+
+print("a. Entire list: ", world_wonders_list)
+print("b. 4th index is: ", world_wonders_list[3])
+world_wonders_list[1] = "Great Wall of China"
+del world_wonders_list[4]
+print("c. Updated list: ", world_wonders_list)
+print("d. Last index is: ", world_wonders_list[-1])
