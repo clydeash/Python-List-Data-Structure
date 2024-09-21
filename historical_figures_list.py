31f9860bc6e1f44c193a0dea52d07b8a5fadc74d
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 14:12:46 +0800

    The list of 10 most historical figures in the world

---
+++ historical_figures_list.py
@@ -0,0 +1,9 @@
+historical_figures = ["Abraham Lincoln", "Cleopatra", "Alexander the Great", "Julius Caesar", "Mahatma Gandhi", "Queen Elizabeth I", "Christopher Columbus", "Leonardo da Vinci", "Joan of Arc", "Napoleon Bonaparte"]
+
+print("a. Entire list: ", historical_figures)
+print("b. 8th index is: ", historical_figures[7])
+historical_figures[3] = "Nelson Mandela"
+del historical_figures[6]
+print("c. Updated list: ", historical_figures)
+print("d. Sliced portion: ", historical_figures[4:7])
+print("e. Last index is: ", historical_figures[-1])
