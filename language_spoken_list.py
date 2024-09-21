e62a588e841f5aea34ae45e8fcddbb092ac5ebfb
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 09:21:33 +0800

    The list of 20 spoken language all over the world

---
+++ language_spoken_list.py
@@ -0,0 +1,9 @@
+spoken_language_names = ["English", "Mandarin", "Hindi", "French", "Arabic", "Bengali", "Russian", "Portuguese", "Urdu", "Indonesian", "German", "Japanese", "Nigerian Pidgin", "Marathi", "Telugu", "Turkish", "Tamil", "Vietnamese", "Korean", "Italian", "Burmese"]
+
+print("a. Entire list: ",spoken_language_names)
+print("b. 13th index is : ",spoken_language_names[13])
+spoken_language_names[10] = "Spanish"
+del spoken_language_names[16]
+print("d. Updated list: ",spoken_language_names)
+print("e. Sliced portion: ", spoken_language_names[6:11])
+print("f. Last index is: ", spoken_language_names[-1])
