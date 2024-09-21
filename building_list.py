5ec136e5945e8f301a1efe4594c801203f74abf0
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 15:23:17 +0800

    The List of 8 most famous building

---
+++ building_list.py
@@ -0,0 +1,8 @@
+building_list = ["Taj Mahal", "Empire State Building", "Burj Khalifa", "Sydney Opera House", "Colosseum", "Big Ben", "Petronas Towers", "Leaning Tower of Pisa"]
+
+print("a. Entire list: ", building_list)
+print("b. 5th index is: ", building_list[5])
+building_list[1] = "Eiffel Tower"
+del building_list[5]
+print("c. Updated list: ", building_list)
+print("d. Last index is: ", building_list[-1])
