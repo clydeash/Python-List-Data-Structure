78c4a75118807b63d5664ba21e5b6d6e4d554730
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 14:20:08 +0800

    The list of 8 famous invention in the history

---
+++ invetion_list.py
@@ -0,0 +1,8 @@
+invention_list = ["Telephone", "Airplane", "Computer", "Penicillin", "Television", "Electricity", "Automobile", "Internet"]
+
+print("a. Entire list: ", invention_list)
+print("b. 6th index is: ", invention_list[5])
+invention_list[1] = "Lightbulb"
+del invention_list[4]
+print("c. Updated list: ", invention_list)
+print("d. Last index is: ", invention_list[-1])
