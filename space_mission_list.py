5f59e1e08bac6f8f565ec4274d29799d2f81f210
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 22:22:10 +0800

    The list of 10 successful mission in space

---
+++ space_mission_list.py
@@ -0,0 +1,8 @@
+space_missions_list = ["Vostok 1", "Apollo 13", "Sputnik 1", "SpaceX Crew Dragon Demo-2", "Hubble Space Telescope", "Chandrayaan-2", "Voyager 1", "Curiosity Rover", "New Horizons", "ISS Expedition 64"]
+
+print("a. Entire list: ", space_missions_list)
+print("b. 7th index is: ", space_missions_list[6])
+space_missions_list[3] = "Apollo 11"
+del space_missions_list[5]
+print("c. Updated list: ", space_missions_list)
+print("d. Last index is: ", space_missions_list[-1])
