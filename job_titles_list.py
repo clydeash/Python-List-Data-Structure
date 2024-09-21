245946ae2b0c7f2a7f8c89cee13e77978250f255
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 16:13:30 +0800

    The list of 10 In demand Job titles

---
+++ job_titles_list.py
@@ -0,0 +1,8 @@
+job_titles_list = ["Teacher", "Doctor", "Engineer", "Nurse", "Artist", "Accountant", "Chef", "Lawyer", "Software Developer", "Manager"]
+
+print("a. Entire list: ", job_titles_list)
+print("b. 6th index is: ", job_titles_list[5])
+job_titles_list[4] = "Software Engineer"
+del job_titles_list[8]
+print("c. Updated list: ", job_titles_list)
+print("d. Last index is: ", job_titles_list[-1])
