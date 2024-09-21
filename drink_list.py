28da45a2d90630bf0a9e77af634db86e88bb07cc
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 20:34:18 +0800

    The List of 8 Popular drinks in hotels

---
+++ drink_list.py
@@ -0,0 +1,8 @@
+drink_list = ["Tea", "Lemonade", "Soda", "Juice", "Water", "Hot Chocolate", "Milk", "Smoothie"]
+
+print("a. Entire list: ", drink_list)
+print("b. 4th index is: ", drink_list[3])
+drink_list[2] = "Coffee"
+del drink_list[6]
+print("c. Updated list: ", drink_list)
+print("d. Last index is: ", drink_list[-1])
