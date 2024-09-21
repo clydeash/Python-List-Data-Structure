04d2163f1fa62368a89bc92849dc1ab7402a1379
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 14:51:41 +0800

    The list of 7 continents in the world

---
+++ continent_list.py
@@ -0,0 +1,8 @@
+continent_list = ["North America", "Africa", "South America", "Asia", "Australia", "Antarctica", "Europe"]
+
+print("a. Entire list: ", continent_list)
+print("b. 4th index is: ", continent_list[3])
+continent_list[1] = "Africa"
+del continent_list[5]
+print("c. Updated list: ", continent_list)
+print("d. Last index is: ", continent_list[-1])
