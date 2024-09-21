dcbe5652bfe36b4fa4185bc040e3f7d995e20e6b
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 22:00:42 +0800

    The list of 7 most famous active volcanoes

---
+++ volcano_list.py
@@ -0,0 +1,8 @@
+volcano_list = ["Mount Kilimanjaro", "Mount Fuji", "Mount St. Helens", "Mount Etna", "Mount Rainier", "Mauna Loa", "Krakatoa"]
+
+print("a. Entire list: ", volcano_list)
+print("b. 4th index is: ", volcano_list[3])
+volcano_list[2] = "Mount Vesuvius"
+del volcano_list[4]
+print("c. Updated list: ", volcano_list)
+print("d. Last index is: ", volcano_list[-1])
