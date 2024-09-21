b56202f84ae4819e98291e8e44a6034af5086cc2
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 09:03:58 +0800

    List of the 12 famous authors

---
+++ author_list.py
@@ -0,0 +1,9 @@
+author_names = ["Ernest Hemingway", "J.K Rowling", "William Shakespeare", "Ayhn Rand", "Edgar Allan Poe", "George Eliot", "William Faulkner", "James Joyce", "Leo Tolstoy", "Lewis Carroll", "Oscar Wilde", "Sylvia Plath", "Maya Angelou"]
+
+print("a. Entire list: ",author_names)
+print("b. 6th index is : ",author_names[6])
+author_names[4] = "Mark Twain"
+del author_names[10]
+print("d. Updated list: ",author_names)
+print("e. Sliced portion: ", author_names[3:8])
+print("f. Last index is: ", author_names[-1])
