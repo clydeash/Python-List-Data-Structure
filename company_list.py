947b53d0725911f4b8c2655db8379b6f43f6bf38
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 12:54:08 +0800

    List of 10 most famous company in the world

---
+++ company_list.py
@@ -0,0 +1,8 @@
+company_names = ["Amazon", "Google", "Nike", "Adidas", "Ikea", "Microsoft", "Starbucks", "Sony", "Cisco", "Audi", "Toyota"]
+
+print("a. Entire list: ",company_names)
+print("b. 7th index is : ", company_names[7])
+company_names[5] = "Apple"
+del company_names[8]
+print("d. Updated list: ",company_names)
+print("f. Last index is: ", company_names[-1])
