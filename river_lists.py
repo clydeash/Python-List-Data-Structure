6f9cb2946746e6e4c04dfa191e80afdac66893ca
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 21:49:10 +0800

    The 10 famous river in the world

--- river_lists.py
+++
@@ -1,8 +0,0 @@
-river_list = ["Amazon River", "Zambezi River", "Yangtze River", "Mississippi River", "Ganges River", "Danube River", "Mekong River", "Volga River", "Rhine River", "Yukon River"]
-
-print("a. Entire list: ", river_list)
-print("b. 6th index is: ", river_list[5])
-river_list[7] = "Nile River"
-del river_list[8]
-print("c. Updated list: ", river_list)
-print("d. Last index is: ", river_list[-1])
