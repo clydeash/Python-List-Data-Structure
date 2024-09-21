aaa591942b6f634a8b0478dae5c33a5975fe4b80
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 16:20:32 +0800

    The list of 5 famous oceans.

---
+++ ocean_list.py
@@ -0,0 +1,10 @@
+ocean_list = ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Southern Ocean", "Pacific ocean"]
+
+print("a. Entire list: ", ocean_list)
+print("b. 3rd index is: ", ocean_list[2])
+
+ocean_list[1] = "Pacific Ocean"
+del ocean_list[3]
+
+print("c. Updated list: ", ocean_list)
+print("d. Last index is: ", ocean_list[-1])
