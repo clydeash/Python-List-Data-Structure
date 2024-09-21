bdd5da8cd042f815c470b3121cdffce4608aa934
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 20:16:32 +0800

    The List of 10 Currency in the World

---
+++ currency_list.py
@@ -0,0 +1,8 @@
+currency_list = ["US Dollar", "Kuwaiti Dinar", "Japanese Yen", "British Pound", "Swiss Franc", "Australian Dollar", "Canadian Dollar", "Chinese Yuan", "Indian Rupee", "South African Rand"]
+
+print("a. Entire list: ", currency_list)
+print("b. 4th index is: ", currency_list[3])
+currency_list[6] = "Euro"
+del currency_list[8]
+print("c. Updated list: ", currency_list)
+print("d. Last index is: ", currency_list[-1])
