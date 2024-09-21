e8bc2f320c0debe4006cf9bb4478d77e48478797
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 22:05:36 +0800

    The list of 10 most attractive waterfalls

---
+++ waterfall_list.py
@@ -0,0 +1,10 @@
+waterfall_list = ["Victoria Falls", "Angel Falls", "Iguazu Falls", "Huangguoshu Falls", "Gullfoss", "Yosemite Falls", "Dettifoss", "Plitvice Falls", "Kaieteur Falls", "Jog Falls"]
+
+print("a. Entire list: ", waterfall_list)
+print("b. 8th index is: ", waterfall_list[7])
+
+waterfall_list[4] = "Niagara Falls"
+del waterfall_list[8]
+
+print("c. Updated list: ", waterfall_list)
+print("d. Last index is: ", waterfall_list[-1])
