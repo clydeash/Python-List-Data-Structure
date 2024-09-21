d6aea62ec363e862eca9434f958240c60a576657
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 15:41:22 +0800

    The list of 10 Important tools in carpentry

---
+++ tools_list.py
@@ -0,0 +1,8 @@
+tool_list = ["Shovel", "Saw", "Drill", "Chisel", "Plane", "Level", "Screwdriver", "Wrench", "Tape Measure", "Mallet"]
+
+print("a. Entire list: ", tool_list)
+print("b. 4th index is: ", tool_list[3])
+tool_list[6] = "Hammer"
+del tool_list[4]
+print("c. Updated list: ", tool_list)
+print("d. Last index is: ", tool_list[-1])
