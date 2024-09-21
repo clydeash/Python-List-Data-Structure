ceaf95c9bdc9675aa0e7425e10c47c0630a42786
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 22:13:37 +0800

    This is the list of Famous Bridge in the world

---
+++ famous_bridge_list.py
@@ -0,0 +1,8 @@
+famous_bridge_list = ["Brooklyn Bridge", "Tower Bridge", "Sydney Harbour Bridge", "Great Belt Bridge ", "Millau Viaduct", "London Bridge", "Rialto Bridge", "Akashi Kaikyo Bridge"]
+
+print("a. Entire list: ", famous_bridge_list)
+print("b. 3rd index is: ", famous_bridge_list[2])
+famous_bridge_list[5] = "Golden Gate Bridge"
+del famous_bridge_list[6]
+print("c. Updated list: ", famous_bridge_list)
+print("d. Last index is: ", famous_bridge_list[-1])
