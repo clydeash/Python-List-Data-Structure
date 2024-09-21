6fc3dfee0f774fa485613fc9aabfb8a58b00b08b
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 14:06:03 +0800

    The list of 7 Animal Species in the Jungle

---
+++ animal_species_list.py
@@ -0,0 +1,8 @@
+animal_species_names = ["African Elephant", "Tiger", "Blue whale", "Golden Lion Tamarin", "Komodo dragon", "Giant panda", "Hummingbird", "Octopus", "Monarch Butterfly"]
+
+print("a. Entire list: ", animal_species_names)
+print("b. 2nd index is : ", animal_species_names[2])
+animal_species_names[5] = "Gorilla"
+del animal_species_names[6]
+print("c. Updated list: ", animal_species_names)
+print("d. Last index is: ", animal_species_names[-1])
