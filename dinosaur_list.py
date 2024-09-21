e9dc601d0915b335dc3331c14d11b66a7731aaf0
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-09-21 21:23:30 +0800

    The list of 8 Legendary Dinosaurs

---
+++ dinosaur_list.py
@@ -0,0 +1,8 @@
+dinosaur_list = ["Iguanodon", "Velociraptor", "Triceratops", "Stegosaurus", "Brachiosaurus", "Spinosaurus", "Ankylosaurus", "Pterodactyl"]
+
+print("a. Entire list: ", dinosaur_list)
+print("b. 4th index is: ", dinosaur_list[3])
+dinosaur_list[5] = "Tyrannosaurus Rex"
+del dinosaur_list[6]
+print("c. Updated list: ", dinosaur_list)
+print("d. Last index is: ", dinosaur_list[-1])
